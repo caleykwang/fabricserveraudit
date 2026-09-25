e8b914951c8dd6a32837b93204c89040270a9a48
caleykwang <123776456+caleykwang@users.noreply.github.com>
2026-09-24 23:44:29 -0400

    Add network handler connection accessor

---
+++ src/main/java/dev/caley/fabricserveraudit/mixin/ServerCommonNetworkHandlerAccessor.java
@@ -0,0 +1,12 @@
+package dev.caley.fabricserveraudit.mixin;
+
+import net.minecraft.network.ClientConnection;
+import net.minecraft.server.network.ServerCommonNetworkHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ServerCommonNetworkHandler.class)
+public interface ServerCommonNetworkHandlerAccessor {
+    @Accessor("connection")
+    ClientConnection fabricserveraudit$getConnection();
+}
